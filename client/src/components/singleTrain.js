304f185f551397dc0cedca0d9b0a88f30b915d7c
m2karthi <m2karthi20@gmail.com>
2023-08-19 12:38:20 +0530

    displayed details in card single train

--- client/src/components/singleTrain.js
+++ client/src/components/singleTrain.js
@@ -27,11 +27,14 @@ function SingleTrainPage() {
     return (
         <div style={{ display: 'flex',flexDirection: 'column',justifyContent: 'center',alignItems: 'center',  height: '100vh' }}>
             <h2>Train Details</h2>
-            <Card>
+            <Card style={{ width: '400px', borderRadius: '10px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)' }}>
                 <CardContent>
                     <Typography variant="h5" component="div">{train.trainName}</Typography>
                     <Typography variant="subtitle1" color="textSecondary">Train Number: {train.trainNumber}</Typography>
                     <Typography variant="body2">Departure Time: {train.departureTime.Hours}:{train.departureTime.Minutes}</Typography>
+                    <Typography variant="body2">Seats Available - Sleeper: {train.seatsAvailable.sleeper}, AC: {train.seatsAvailable.AC}</Typography>
+                    <Typography variant="body2">Price - Sleeper: {train.price.sleeper}, AC: {train.price.AC}</Typography>
+                    <Typography variant="body2">Delayed By: {train.delayedBy} minutes</Typography>
                 </CardContent>
             </Card>
         </div>
