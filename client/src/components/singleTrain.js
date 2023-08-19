bf1f6502bcdaf0d6a69a10da6f22099506d413f1
m2karthi <m2karthi20@gmail.com>
2023-08-19 12:54:42 +0530

    updated card color

--- client/src/components/singleTrain.js
+++ client/src/components/singleTrain.js
@@ -27,13 +27,28 @@ function SingleTrainPage() {
     return (
         <div style={{ display: 'flex',flexDirection: 'column',justifyContent: 'center',alignItems: 'center',  height: '100vh' }}>
             <h2>Train Details</h2>
-            <Card style={{ width: '400px', borderRadius: '10px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)' }}>
+            <Card style={{
+                width: '400px',
+                borderRadius: '10px',
+                boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
+                background: 'linear-gradient(135deg, #f2a9d3, #83dbd7)',
+                fontFamily: 'Georgia, serif',
+                color: '#333'
+            }}>
                 <CardContent>
                     <Typography variant="h5" component="div">{train.trainName}</Typography>
                     <Typography variant="subtitle1" color="textSecondary">Train Number: {train.trainNumber}</Typography>
                     <Typography variant="body2">Departure Time: {train.departureTime.Hours}:{train.departureTime.Minutes}</Typography>
-                    <Typography variant="body2">Seats Available - Sleeper: {train.seatsAvailable.sleeper}, AC: {train.seatsAvailable.AC}</Typography>
-                    <Typography variant="body2">Price - Sleeper: {train.price.sleeper}, AC: {train.price.AC}</Typography>
+                    <Typography variant="body2">Seats Available:</Typography>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <div>Sleeper: {train.seatsAvailable.sleeper}</div>
+                        <div>AC: {train.seatsAvailable.AC}</div>
+                    </div>
+                    <Typography variant="body2">Price:</Typography>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <div>Sleeper: {train.price.sleeper}</div>
+                        <div>AC: {train.price.AC}</div>
+                    </div>
                     <Typography variant="body2">Delayed By: {train.delayedBy} minutes</Typography>
                 </CardContent>
             </Card>
