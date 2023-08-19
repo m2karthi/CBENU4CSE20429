ad2fa77fad419b67386cff63ec8cd24478ab77c5
m2karthi <m2karthi20@gmail.com>
2023-08-19 11:50:14 +0530

    completed apis for frontend

--- server/index.js
+++ server/index.js
@@ -44,6 +44,85 @@ async function getAuthToken() {
     }
 }
 
+// Fetch train details
+async function fetchTrainDetails() {
+    try {
+        const response = await axios.get(`${JOHN_DOE_BASE_URL}/trains`, {
+            headers: {
+                Authorization: `Bearer ${authToken}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching train details:', error.message);
+        return [];
+    }
+}
+
+// Process and filter train data
+async function processTrainData() {
+    const allTrains = await fetchTrainDetails();
+    const currentTime = new Date();
+
+    const filteredTrains = allTrains.filter(train => {
+        const departureTime = new Date(
+            currentTime.getFullYear(),
+            currentTime.getMonth(),
+            currentTime.getDate(),
+            train.departureTime.Hours,
+            train.departureTime.Minutes,
+            train.departureTime.Seconds
+        );
+        const timeDifference = departureTime - currentTime;
+        const minutesDifference = Math.floor(timeDifference / (1000 * 60));
+        return minutesDifference >= 30;
+    });
+
+    filteredTrains.sort((a, b) => {
+        if (a.price.AC === b.price.AC) {
+            if (a.price.sleeper === b.price.sleeper) {
+                return b.seatsAvailable.sleeper - a.seatsAvailable.sleeper;
+            }
+            return a.price.sleeper - b.price.sleeper;
+        }
+        return a.price.AC - b.price.AC;
+    });
+
+    return filteredTrains;
+}
+
+
+
+// CORS setup to allow requests from the frontend
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.get('/train-schedule', async (req, res) => {
+    try {
+        const processedTrains = await processTrainData();
+        res.json(processedTrains);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.get('/train-schedule/:trainNumber', async (req, res) => {
+    const { trainNumber } = req.params;
+    try {
+        const allTrains = await fetchTrainDetails();
+        const selectedTrain = allTrains.find(train => train.trainNumber === trainNumber);
+        if (!selectedTrain) {
+            return res.status(404).json({ error: 'Train not found' });
+        }
+        res.json(selectedTrain);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 
 app.listen(PORT, () => {
